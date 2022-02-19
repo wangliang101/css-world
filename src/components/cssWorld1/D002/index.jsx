4258b75b4241169fea61ba1694c2237eb639d77e
wliang <wliang@infervision.com>
2022-02-19 21:34:31 +0800

    [add] css demo提交模版

---
+++ src/components/cssWorld1/D002/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const D002 = () => {
+  console.log('111');
+
+  return(
+    <div>
+      sdd
+    </div>
+  )
+}
+
+export default D002;
--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import './index.css';
+
+import D002 from '../../components/cssWorld1/D002';
+
+const { TabPane } = Tabs;
 
 const Home = () => {
 
   return(
-    <div>
-      home
+    <div className='home_wrap'>
+      <Tabs defaultActiveKey="D002" tabPosition='left' style={{ height: '100%' }}> 
+        <TabPane tab="D002" key="D002">
+          <D002 />
+        </TabPane>
+      </Tabs>
     </div>
   )
 }
